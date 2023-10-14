967d922910872d8764861e055aaa1efcbfd9853d
Riley B <rilobreske@gmail.com>
2023-10-14 09:15:42 -0400

    oct 14 update

---
+++ frontend/src/screens/HomeScreen.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+import { Row, Col } from 'react-bootstrap'
+import Product from '../components/Product'
+import axios from 'axios'
+
+const HomeScreen = () => {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products')
+      setProducts(data)
+    }
+
+    fetchProducts()
+  }, [])
+
+  return (
+    <>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((products) => (
+          <Col key={products.id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={products} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  )
+}
+
+export default HomeScreen
