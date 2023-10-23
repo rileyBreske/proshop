a8c4de30fc5031803ac46591e4dc121e15ea4413
Riley B <rilobreske@gmail.com>
2023-10-23 12:21:06 -0400

    error handling updates

---
+++ backend/controllers/productControllers.js
@@ -0,0 +1,1 @@
+const getProducts = async (req, res) => {}
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -19,8 +19,8 @@ router.get(
     if (product) {
       res.json(product)
     }
-
-    res.status(404).json({ message: 'Product not found' })
+    res.status(404)
+    throw new Error('Resource not found')
   })
 )
 
