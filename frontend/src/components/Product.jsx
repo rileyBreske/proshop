245e30166eb5e15dd005a90b1c6c35e286fe0bd2
Riley B <rilobreske@gmail.com>
2023-10-23 12:20:45 -0400

    products images trim to have uniformity in cards

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,19 +1,20 @@
+import React from 'react'
 import { Card } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
-import Rating from './Rating'
-
-import React from 'react'
+import Rating from './Rating' // Assuming you have a Rating component
 
 const Product = ({ product }) => {
   return (
     <Card className='my-3 p-3 rounded'>
       <Link to={`/product/${product._id}`}>
-        <Card.Img src={product.image} variant='top' />
+        <div className='product-image'>
+          <Card.Img src={product.image} variant='top' alt={product.name} />
+        </div>
       </Link>
 
       <Card.Body>
         <Link to={`/product/${product._id}`}>
-          <Card.Title as='div' class='product-title'>
+          <Card.Title as='div' className='product-title'>
             <strong>{product.name}</strong>
           </Card.Title>
         </Link>
