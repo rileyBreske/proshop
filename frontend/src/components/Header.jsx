b68c8a6d36ddc226c1f61a78ee13ea6087354474
Riley B <rilobreske@gmail.com>
2023-10-23 12:19:54 -0400

    logo change and footer and header styling changes

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Navbar, Nav, Container } from 'react-bootstrap'
 import { FaShoppingCart, FaUser } from 'react-icons/fa'
 import { LinkContainer } from 'react-router-bootstrap'
-import logo from '../assets/logo.png'
+import logo from '../assets/logo3.png'
 import NavDropdown from 'react-bootstrap/NavDropdown'
 
 const Header = () => {
